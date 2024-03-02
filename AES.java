ee0d688d7ee028fcbf1e985aa658449f467c6561
kanowl <viktor182@gmail.com>
2024-03-01 20:52:47 -0500

    Add files via upload

---
+++ AES.java
@@ -0,0 +1,35 @@
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+/**
+ *
+
+ */
+public class AES {
+    public static void main(String[] args) {
+        try {
+            final String claveEncriptacion = "sistemasdistribuidos";
+            String datosOriginales = "Hola mund";
+
+            Encriptador encriptador = new Encriptador();
+
+            String encriptado = encriptador.encriptar(datosOriginales, claveEncriptacion);
+            String desencriptado = encriptador.desencriptar(encriptado, claveEncriptacion);
+
+            System.out.println("Cadena Original: " + datosOriginales);
+            System.out.println("Escriptado     : " + encriptado);
+            System.out.println("Desencriptado  : " + desencriptado);
+
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
